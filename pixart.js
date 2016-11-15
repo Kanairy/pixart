23bbeef22a749f2cd841b570f30e1a15a4da127e
PakkuDon <pakku.pd@gmail.com>
2016-11-15 15:10:41 +1100

    Add stamp functionality

--- pixart.js
+++ pixart.js
@@ -2,14 +2,22 @@ var NUM_SQUARES = 20;
 
 // Grab DOM elements
 var $brush = $('.brush');
-var $form = $('#form');
+var $stamp = $('.stamp');
 var $colorField = $('#color-field');
+var $stampField = $('#stamp-field');
 
 // Set color of brush box to input color
-$form.submit(function(e) {
+$('#brush-form').submit(function(e) {
   e.preventDefault();
 
   $brush.css('background-color', $colorField.val());
+});
+
+// Set stamp image
+$('#stamp-form').submit(function(e) {
+  e.preventDefault();
+  $stamp.css('background-image', 'url(' + $stampField.val() + ')');
+  console.log($stampField.val());
 });
 
 // Append squares to body
@@ -18,8 +26,19 @@ for (var i = 0; i < NUM_SQUARES; i++) {
   $('body').append($square);
 }
 
-// Change square colour to brush colour
+// Change square fill
 $('body').on('mouseover', '.square', function(e) {
-  var newColour = $brush.css('background-color');
-  $(e.target).css('background-color', newColour);
+  var tool = $('input[name="tool"]:checked').val();
+  var $target = $(e.target);
+
+  // If brush selected, set square color
+  if (tool === "brush") {
+    var newColour = $brush.css('background-color');
+    $target.css('background-color', newColour)
+      .css('background-image', '');
+  }
+  // Else, stamp square
+  else {
+    $target.css('background-image', $stamp.css('background-image'));
+  }
 });
