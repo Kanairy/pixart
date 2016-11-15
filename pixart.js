89df62c7b8eb0796bb0e54a48926f9bebd497de0
Catherine Gracey <catherine.gracey@gmail.com>
2016-11-15 14:26:03 +1100

    Pressing enter key inside the input field triggers the set-color button

--- pixart.js
+++ pixart.js
@@ -1,7 +1,14 @@
 function setColor(event){
-  event.preventDefault()
+  event.preventDefault();
   var color = $("#color-field").val();
   var $box = $($('.brush')[0]);
   $box.css("background-color", color)
 }
 $('#set-color').on("click", setColor);
+
+function checkKeypress(event){
+  if (event.keyCode == 13){
+    $('#set-color').click();
+  }
+}
+$('#color-field').on("keypress", checkKeypress);
