1ad23ba88bc127604fbcdcbc96fd443e6c6ff380
Tim Walter <twwalter@gmail.com>
2016-11-15 14:36:33 +1100

    Stage2: Color change on enter

--- pixart.js
+++ pixart.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
   $('#set-color').click(function(e) {
     e.preventDefault();
+    
+    // change color on button press
     brush_color = $('#color-field').val();
     $('.brush').css('background-color', brush_color);
   })
+
+  // change color on enter
+  $('#color-field').keypress(function (e) {
+  if (e.which == 13) {
+    $('#set-color').submit();
+  }
+  });
 });
