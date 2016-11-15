c92ad1346837a8b9c7df4a267cbbbeb697f64ba6
CKS <ckseng86@gmail.com>
2016-11-15 14:32:12 +1100

    create 20 divs with square class - commit 3

--- pixart.js
+++ pixart.js
@@ -4,3 +4,8 @@ $('#set-color').on('click', function(event) {
   var newColor = $('#color-field').val()
   $('.brush').css('background-color', newColor);
 })
+
+
+for (var i = 0; i < 20; i++) {
+  $('body').append($('<div>').addClass('square'));
+}
