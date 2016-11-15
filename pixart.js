f8e49c562c110e581c82bf65256ec108bc1274d7
Paul Williams <paul@pdwilliams.net>
2016-11-15 14:24:42 +1100

    create square divs

--- pixart.js
+++ pixart.js
@@ -1,6 +1,12 @@
 var $setColorBtn = $('#set-color');
 var $brushDiv = $('.brush');
 var $colorInput = $('#color-field');
+var $body = $('body');
+
+for (var i = 0; i < 20; i++) {
+  var $squareDiv = $('<div class="square">');
+  $body.append($squareDiv);
+}
 
 var changeColor = function(e) {
   e.preventDefault();
