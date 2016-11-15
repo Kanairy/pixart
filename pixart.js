56f317620bb0e11140b97ce2d55e29a7313231df
bruno <bruno.rampinelli.rota@gmail.com>
2016-11-15 14:59:18 +1100

    second poit work

--- pixart.js
+++ pixart.js
@@ -7,3 +7,11 @@ $('#set-color').click (function(event){
 })
 
 // The same thing should happen when I press the enter key from inside the input field
+
+
+
+$( "#set-color" ).keypress(function(event) {
+  event.preventDefault();
+  var input = $('#color-field').val();
+  $(".brush").css("background-color", input);
+});
