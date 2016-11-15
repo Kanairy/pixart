bee5292e0537b076f43745875241be83cf04812a
Micah Boyd <micahhope@yahoo.com>
2016-11-15 15:52:17 +1100

    nearly completed ajax poster request to change brush to poster image

--- pixart.js
+++ pixart.js
@@ -1,21 +1,38 @@
+var input = $('#color-field').val();
+var stampInput = $('#stamp-field').val();
+
 
 $('#set-color').click(function(event){
   event.preventDefault()
-  var input = $('#color-field').val();
   $('.brush').css(
     "background-color", input)
 })
 
-
-
 var makeSquares = function(){
-  for (var i = 0; i < 20; i++) {
+  for (var i = 0; i < 5000; i++) {
     var $square = $('<div>').addClass('square');
     $('body').append($square);
   }
 }();
 
 $('.square').hover(function(event){
-  var input = $('#color-field').val();
-  $(event.target).css( "background-color", input)
+  var brush = $('.brush').css(
+    "background-color")
+  $(event.target).css( "background-color", brush)
+})
+
+$('#set-stamp').click(function(event){
+  event.preventDefault()
+  $.ajax({
+    url: 'http://www.omdbapi.com/',
+    data: { i: stampInput }
+  }).done(function(response){
+    console.log('received');
+    var movie = response;
+
+    $('.brush').css(
+      "background-image", 'image(' + movie.Poster + ')')
+
+    });
+
 })
