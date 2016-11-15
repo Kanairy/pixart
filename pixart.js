6ad6a78e4c31a9c474e6ad6160a1272ac25ba0d7
James Mah <junyeumah@gmail.com>
2016-11-15 14:32:54 +1100

    creation of 20 square class divs

--- pixart.js
+++ pixart.js
@@ -4,3 +4,8 @@ $('#form').submit(function(event){
   var input = $('#color-field').val();
   $('.brush').css('backgroundColor',input);
 });
+
+for (var i=0; i<20; i++) {
+  $square = $('<div>').addClass('square');
+  $('body').append($square);
+}
