487932f516736d3e043eabdee89b993c174633d2
Jason Pompon <jasonpompon@gmail.com>
2016-11-15 14:40:54 +1100

    commit 1

---
+++ pixart.js
@@ -0,0 +1,9 @@
+
+  var $button = $('#set-color');
+  var input = $('#color-field').val('enter a color');
+  var $brush = $('.brush');
+
+  $button.click(function(e) {
+    e.preventDefault();
+    $brush.css('background-color', input.val());
+  });
