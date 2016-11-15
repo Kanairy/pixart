1dd95d39fb1ba486f4360ce7a1272242b6ccef01
Tim Walter <twwalter@gmail.com>
2016-11-15 14:58:46 +1100

    Stage3: 10 square divs

--- pixart.js
+++ pixart.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('#set-color').click(function(e) {
     e.preventDefault();
-    
+
     // change color on button press
     brush_color = $('#color-field').val();
     $('.brush').css('background-color', brush_color);
@@ -13,4 +13,10 @@ $(document).ready(function() {
     $('#set-color').submit();
   }
   });
+
 });
+
+// create 20 square class divs
+for (var i=0; i<20; i++) {
+    $("body").append("<div class='square'></div>");
+};
