47a564484d55f97be4e588256b417574b01c4e42
Pedro Araujo <ppteixeira@outlook.com>
2016-11-15 14:59:02 +1100

    Commit 3

--- pixart.js
+++ pixart.js
@@ -1,11 +1,22 @@
+//commit 1
 $('#set-color').on('click', function(event) {
   event.preventDefault();
   var color = $('#color-field').val();
   $('.brush').css("background", color);
 })
 
+//commit 2
 $('color-field').bind("enterKey",function(event){
   event.preventDefault();
   var color = $('#color-field').val();
   $('.brush').css("background", color);
 });
+
+// commit 3
+
+var create20Divs = function() {
+  for (var i = 0; i < 20; i++) {
+    $('.controls').append( $('<div>',{class: 'square'}) );
+  }
+};
+create20Divs();
