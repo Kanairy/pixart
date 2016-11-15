86c582ebbedc2c6f89df5a8e3a3253deefac358c
charithperera <charith.p@gmail.com>
2016-11-15 14:29:08 +1100

    Commit 3

--- pixart.js
+++ pixart.js
@@ -2,8 +2,18 @@ $(document).ready(function(){
   var $setColorBtn = $("#set-color");
   var $brushBox = $(".brush");
 
+  // COMMIT 1 & 2: Change color on button also
+  // works on enter from within input field as
+  // it's in a form an enter triggers the submit
+  // buttons click which is captured here
   $setColorBtn.on("click", function(e) {
     e.preventDefault();
     $brushBox.css({"background-color": "#"+$("#color-field").val()})
   })
+
+  // COMMIT 3
+  for (var i = 0; i < 20; i++) {
+    $("<div>", { class: "square" }).appendTo("body");
+  }
+
 });
