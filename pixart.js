8de85dac4e09b74d8d00f1c75113800c04aa3dc4
Catherine Gracey <catherine.gracey@gmail.com>
2016-11-15 15:12:51 +1100

    Stamp tool stamps and toggles with paint tool

--- pixart.js
+++ pixart.js
@@ -1,22 +1,59 @@
+var mode = "paint";
+
+//Keypress events for input fields
+function checkKeypress(event){
+  if (event.keyCode == 13){
+    if (event.target.id === "color-field"){
+      $('#set-color').click();
+    } else {
+      $('#set-stamp').click();
+    }
+  }
+}
+$('#color-field').on("keypress", checkKeypress);
+$('#stamp-field').on("keypress", checkKeypress);
+
+//Paint modes
 function setColor(event){
   event.preventDefault();
+  mode = "paint";
   var color = $("#color-field").val();
   var $box = $($('.brush')[0]);
   $box.css("background-color", color)
 }
 $('#set-color').on("click", setColor);
 
-function checkKeypress(event){
-  if (event.keyCode == 13){
-    $('#set-color').click();
-  }
+//Stamp modes
+function setStamp(event){
+  event.preventDefault();
+  mode = "stamp";
+  var image = $("#stamp-field").val();
+  var $box = $($('.stamp')[0]);
+  $box.empty().append( $('<img>',{src: image}) );
 }
-$('#color-field').on("keypress", checkKeypress);
+$('#set-stamp').on("click", setStamp);
 
+//Modify squares
 function colorSquare(event){
   var $square = $(event.target);
   var color = $($('.brush')[0]).css("background-color");
-  $square.css("background-color", color)
+  $square.css("background-color", color);
+}
+
+function stampSquare(event){
+  var $square = $(event.target);
+  var image = $($($('.stamp')[0]).children()[0]).clone();
+  $square.append(image);
+}
+
+function fillSquare(event){
+  $(event.target).empty();
+  $(event.target).css("background-color", "transparent");
+  if (mode === "paint"){
+    colorSquare(event);
+  } else {
+    stampSquare(event);
+  }
 }
 
 (function(){
@@ -24,5 +61,5 @@ function colorSquare(event){
     var $square = $('<div class="square">');
     $('body').append($square);
   }
-  $('.square').on("mouseover", colorSquare);
+  $('.square').on("mouseover", fillSquare);
 })();
