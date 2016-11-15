3f5e775fe71b5ea948463bf8d6325423bb19e3a0
paulmorello <paulmorello1@gmail.com>
2016-11-15 14:44:39 +1100

    appended 20 divs with square class to the page

--- pixart.js
+++ pixart.js
@@ -3,14 +3,20 @@
 $('#set-color').on('click', function(event) {
   event.preventDefault();
 
-  var input = $('#color-field').val();
-  $('.brush').css("background-color", input );
+  var $input = $('#color-field').val();
+  $('.brush').css("background-color", $input );
 })
 
-// change the color of the box when enter is pressed
+// Commit 2 change the color of the box when enter is pressed
 $('#set-color').on('keydown', function(event) {
   event.preventDefault();
-  
-  var input = $('#color-field').val();
-  $('.brush').css("background-color", input );
+
+  var $input = $('#color-field').val();
+  $('.brush').css("background-color", $input );
 })
+
+// commit 3 appending 20 divs of the square class
+for (var i = 0; i <= 20; i++) {
+  var $newDiv = $('<div>').addClass('square');
+  $('#square').append($newDiv);
+}
