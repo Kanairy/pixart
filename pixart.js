2642b3e7693b330e5652994d0adfb4fe98dc6391
Kerrie Batrouney <batrouney@gmail.com>
2016-11-15 15:20:50 +1100

    complete 3/5, comfort 3/5

--- pixart.js
+++ pixart.js
@@ -1,11 +1,14 @@
-var $btn = $('.set-color');
-var $box = $('#brush');
-var $color = $('.color-field');
+var $btn = $('#set-color');
+var $box = $('.brush');
+var $color = $('#color-field');
 
 var changeColor = function(event){
   event.preventDefault();
-  $box.css("background-color","$color");
-
+  $box.css("background", "$color");
 }
 $btn.on("click", changeColor(event));
 $color.on("submit", changeColor(event));
+
+for (i=0; i<20; i++){
+  $('body').append('<div class="square">');
+}
