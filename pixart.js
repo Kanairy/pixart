7786cab55b36208a20bb8e715b7d36d9c32b7b76
Shoshiro <skarita@gmail.com>
2016-11-15 15:50:03 +1100

    update solution

--- pixart.js
+++ pixart.js
@@ -1,26 +1,27 @@
 var setColor = document.getElementById('set-color');
 var boxColor = document.getElementsByClassName('brush');
+var brush = document.querySelector("div.brush");
+var colorField = document.getElementById('color-field');
+var body = document.querySelector("body");
 
-setColor.addEventListener('click', function(event) {
+setColor.addEventListener("click", function() {
+  brush.style.background = colorInput.value;
+});
+
+colorField.addEventListener('enter', function(event) {
   var colorField = document.getElementById('color-field');
   boxColor.backgroundColor = colorField;
 });
 
-setColor.addEventListener('enter', function(event) {
-  var colorField = document.getElementById('color-field');
-  boxColor.backgroundColor = colorField;
-});
+for(var i = 0; i < 20; i++) {
+  var div = document.createElement("div");
+  div.className = "square";
+  body.appendChild(div);
+}
 
+var squares = document.querySelectorAll("div.square");
 
-var board = function() {
-  for(var i = 0; i < 20; i++){
-    var div = document.createElement("div");
-    div.className = "square";
-    document.body.appendChild(div);
-  }
-};
-
-board.addEventListener('click', function(event) {
+squares.addEventListener('click', function(event) {
   event.target.style.backgroundColor = colorField;
 });
 
