83d19faab5a33de669123f26d163bd18dfaaa31e
Paul Williams <paul@pdwilliams.net>
2016-11-15 14:56:27 +1100

    switch between brush and stamp

--- pixart.js
+++ pixart.js
@@ -2,6 +2,9 @@ var $setColorBtn = $('#set-color');
 var $brushDiv = $('.brush');
 var $colorInput = $('#color-field');
 var $canvas = $('.canvas');
+var $toolSelectBtn = $('#tool-select');
+var $brushControlsDiv = $('.brush-controls');
+var $stampControlsDiv = $('.stamp-controls');
 
 var changeBrushColor = function(e) {
   e.preventDefault();
@@ -12,10 +15,16 @@ var changeColor = function(e) {
   $(e.target).css('background', $brushDiv.css('background'));
 }
 
+var changeTool = function() {
+  $brushControlsDiv.toggle();
+  $stampControlsDiv.toggle();
+}
+
 for (var i = 0; i < 100; i++) {
   var $squareDiv = $('<div class="square">');
   $squareDiv.hover(changeColor);
   $canvas.append($squareDiv);
 }
 
+$toolSelectBtn.click(changeTool);
 $setColorBtn.click(changeBrushColor);
