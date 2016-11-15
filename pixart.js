a594c8cf492f9efbc560d8c6a46aac591388530a
paulmorello <paulmorello1@gmail.com>
2016-11-15 14:33:47 +1100

    User can press enter to change the color

--- pixart.js
+++ pixart.js
@@ -1,8 +1,16 @@
 
+// Commit 1 change the color of the box on click
 $('#set-color').on('click', function(event) {
+  event.preventDefault();
+
+  var input = $('#color-field').val();
+  $('.brush').css("background-color", input );
+})
+
+// change the color of the box when enter is pressed
+$('#set-color').on('keydown', function(event) {
   event.preventDefault();
   
   var input = $('#color-field').val();
-
   $('.brush').css("background-color", input );
 })
