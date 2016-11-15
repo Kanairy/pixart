d351dae2d9566547b7473c0d0963f389c8270005
Chloe Phua <cqphua@gmail.com>
2016-11-15 15:13:29 +1100

    append 20 times

--- pixart.js
+++ pixart.js
@@ -1,3 +1,5 @@
+//COMMIT 1
+//add functionality to change color on button click
 $('#set-color').on('click',function(event) {
   event.preventDefault();
   //change div class background
@@ -5,6 +7,8 @@ $('#set-color').on('click',function(event) {
   $('.brush').css("background",color);
 })
 
+//COMMIT 2
+//add functionality to change color on keypress
 $('#set-color').on('keypress',function(event) {
   event.preventDefault();
   if(event.which === 13) {
@@ -12,3 +16,13 @@ $('#set-color').on('keypress',function(event) {
     $('.brush').css("background",color);
   }
 })
+
+//COMMIT 3
+//create and append 20 divs of the "square" class to the body
+var squares = $("<div>",{
+  class: "square"
+})
+
+for (i=0; i<20; i++) {
+  $('body').append($(squares))
+}
